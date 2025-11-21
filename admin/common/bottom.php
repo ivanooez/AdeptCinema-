b6fb79978c15e0e1a431b2e8ef9323a3eced8e36
ivanooez <148303670+ivanooez@users.noreply.github.com>
2025-11-21 14:31:23 +0000

    Create bottom.php

---
+++ admin/common/bottom.php
@@ -0,0 +1,9 @@
+<?php
+// admin/common/bottom.php
+?>
+</main>
+<footer class="bg-gray-900 p-4 mt-8 border-t border-gray-800 text-center text-sm text-gray-400">
+  Admin &copy; <?php echo date('Y'); ?>
+</footer>
+</body>
+</html>
