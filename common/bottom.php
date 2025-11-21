36e22b78e421afbb649e1d7f2af31268533d24c3
ivanooez <148303670+ivanooez@users.noreply.github.com>
2025-11-21 14:11:52 +0000

    Create bottom.php

---
+++ common/bottom.php
@@ -0,0 +1,9 @@
+<?php
+// common bottom (footer)
+?>
+</main>
+<footer class="bg-gray-900 p-4 mt-8 border-t border-gray-800 text-center text-sm text-gray-400">
+  &copy; <?php echo date('Y').' '.APP_NAME; ?>
+</footer>
+</body>
+</html>
